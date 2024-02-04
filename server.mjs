1e35db406c6020e9b2f9d15e10cf464971f1d7d7
Rafael Aguiar Martins <rafael_aguiar@id.uff.br>
2024-02-04 17:21:07 -0300

    feat: ranking list displayed in respective ranking

--- server.mjs
+++ server.mjs
@@ -10,14 +10,17 @@ const port = 3001;
 app.use(express.json());
 app.use(cors());
 
-app.get('/api/getRankings', async (req, res) => {
+
+//The handler function is responsible for processing the request, executing any necessary logic 
+//(such as fetching data from an API), and sending a response back to the client.
+
+app.get('/api/getRankings', async (req, res) => { //This function takes two parameters: req (request object) and res (response object).
     try {
         const api_key = process.env.REACT_APP_API_KEY;
-        console.log('API Key:', api_key); 
         const response = await fetch(`https://api.sportradar.com/mma/trial/v2/en/rankings.json?api_key=${api_key}`);
         const data = await response.json();
 
-        res.header('Access-Control-Allow-Origin', '*'); // Adjust the origin as needed
+        res.header('Access-Control-Allow-Origin', '*');
         res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
--- src/WeightClassInfo.js
+++ src/WeightClassInfo.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from 'react';
-const api_key = process.env.REACT_APP_API_KEY;
 
 async function getRankings() {
     try {
         console.log("Tried")
         const response = await fetch('http://localhost:3001/api/getRankings');
 
-        
-        // Check if the request was successful
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
@@ -15,7 +12,7 @@ async function getRankings() {
         console.log("fetched")
         const data = await response.json();
         console.log(data);
-        return data;
+        return data.rankings; //rankings array
     } catch (error) {
         console.error('Error fetching rankings:', error);
         throw error;
@@ -29,18 +26,30 @@ function WeightClassInfo ({weightClass}) {
             setRankings(data);
         });
     }, []);  // O segundo argumento vazio indica que este efeito deve ser executado apenas uma vez, após a montagem do componente
+    
+    if (weightClass === "Light-Heavyweight"){
+        weightClass = "light_heavyweight"
+    }
+    const filteredRanking = rankings.filter(ranking => ranking.name === weightClass.toLowerCase()) //ranking is the current element, and everyting after the arrow is the condition of return
 
-    return(
+    
+    return (
         <div>
-            <h1>{weightClass}</h1>
-            <div id="ranking-list">
-                <ol>
-                    <li>Rank: </li>
-                    <li>Name: </li>
-                </ol>
-            </div>
+          <h1>{weightClass === "light_heavyweight" ? "Light-Heavyweight" : weightClass}</h1>
+          <div id="ranking-list">
+            {filteredRanking.map((ranking, index) => ( //ranking represents the current element being processed, and index is its index in the array.
+              <ol key={index}>
+                {ranking.competitor_rankings.map((competitorRanking, subIndex) => ( 
+                  <li key={subIndex}>
+                    {competitorRanking.competitor.name}
+                  </li>
+                ))}
+              </ol>
+            ))}
+          </div>
         </div>
-    );
+      );
+    
 }
 
 export default WeightClassInfo;
